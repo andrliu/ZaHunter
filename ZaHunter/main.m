b9e16bb6d7d603d2d5854f6d93fc950aad813cc2
Andrew Liu <dieseliu321@gmail.com>
2014-11-05 14:58:24 -0800

    Initial Commit

---
+++ ZaHunter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZaHunter
+//
+//  Created by Andrew Liu on 11/5/14.
+//  Copyright (c) 2014 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
